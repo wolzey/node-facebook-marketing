f372593501b98db3c91a9d68161bb0e6b7ea9074
Ethan <ethan@eyeballdivision.com>
2016-04-04 11:16:14 -0600

    missing &

--- libs/index.js
+++ libs/index.js
@@ -93,7 +93,7 @@ function makeFbRequest(path, fields, cb) {
 
 FB.prototype.setLongAccessToken = function(cb) {
   if(_.has(this, 'access_token')) {
-    var requestURL = BASE + '/oauth/access_token?grant_type=fb_exchange_token'+
+    var requestURL = BASE + '/oauth/access_token?grant_type=fb_exchange_token&'+
         'client_id='+this.options.app_id+'&client_secret='+this.options.client_secret+
         'fb_exchange_token='+this.access_token + '&redirect_uri=' +this.options.redirect_uri;
 
